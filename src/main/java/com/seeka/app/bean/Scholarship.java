eabd8b05fc66152cf7d8308b8f97de9d6a024f86
Praveen <praveen.raghuvanshii@gmail.com>
2019-07-16 10:36:52 +0530

    @Praveen , scholarship java class

---
+++ src/main/java/com/seeka/app/bean/Scholarship.java
@@ -0,0 +1,393 @@
+package com.seeka.app.bean;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import java.math.BigInteger;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name = "scholarship", catalog = "seeka_dev_v5")
+public class Scholarship implements java.io.Serializable {
+
+    private BigInteger id;
+    private Country country;
+    private Institute institute;
+    private Level level;
+    private String name;
+    private String amount;
+    private String description;
+    private String student;
+    private String website;
+    private Date createdOn;
+    private Date updatedOn;
+    private String createdBy;
+    private String updatedBy;
+    
+    private String instituteName;
+    private String countryName;
+    private String levelName;
+
+    /**
+     * @return the id
+     */
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    public BigInteger getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     *            the id to set
+     */
+    public void setId(BigInteger id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the country
+     */
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "country_id")
+    public Country getCountry() {
+        return country;
+    }
+
+    /**
+     * @param country
+     *            the country to set
+     */
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    /**
+     * @return the institute
+     */
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "institute_id")
+    public Institute getInstitute() {
+        return institute;
+    }
+
+    /**
+     * @param institute
+     *            the institute to set
+     */
+    public void setInstitute(Institute institute) {
+        this.institute = institute;
+    }
+
+    /**
+     * @return the level
+     */
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "level_id")
+    public Level getLevel() {
+        return level;
+    }
+
+    /**
+     * @param level
+     *            the level to set
+     */
+    public void setLevel(Level level) {
+        this.level = level;
+    }
+
+    /**
+     * @return the name
+     */
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name
+     *            the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the amount
+     */
+    @Column(name = "amount")
+    public String getAmount() {
+        return amount;
+    }
+
+    /**
+     * @param amount
+     *            the amount to set
+     */
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    /**
+     * @return the description
+     */
+    @Column(name = "description")
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description
+     *            the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the student
+     */
+    @Column(name = "student")
+    public String getStudent() {
+        return student;
+    }
+
+    /**
+     * @param student
+     *            the student to set
+     */
+    public void setStudent(String student) {
+        this.student = student;
+    }
+
+    /**
+     * @return the website
+     */
+    @Column(name = "website")
+    public String getWebsite() {
+        return website;
+    }
+
+    /**
+     * @param website
+     *            the website to set
+     */
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_on", length = 19)
+    public Date getCreatedOn() {
+        return this.createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_on", length = 19)
+    public Date getUpdatedOn() {
+        return this.updatedOn;
+    }
+
+    public void setUpdatedOn(Date updatedOn) {
+        this.updatedOn = updatedOn;
+    }
+
+    @Column(name = "created_by", length = 50)
+    public String getCreatedBy() {
+        return this.createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    @Column(name = "updated_by", length = 50)
+    public String getUpdatedBy() {
+        return this.updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((amount == null) ? 0 : amount.hashCode());
+        result = prime * result + ((country == null) ? 0 : country.hashCode());
+        result = prime * result + ((createdBy == null) ? 0 : createdBy.hashCode());
+        result = prime * result + ((createdOn == null) ? 0 : createdOn.hashCode());
+        result = prime * result + ((description == null) ? 0 : description.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((institute == null) ? 0 : institute.hashCode());
+        result = prime * result + ((level == null) ? 0 : level.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((student == null) ? 0 : student.hashCode());
+        result = prime * result + ((updatedBy == null) ? 0 : updatedBy.hashCode());
+        result = prime * result + ((updatedOn == null) ? 0 : updatedOn.hashCode());
+        result = prime * result + ((website == null) ? 0 : website.hashCode());
+        return result;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Scholarship other = (Scholarship) obj;
+        if (amount == null) {
+            if (other.amount != null)
+                return false;
+        } else if (!amount.equals(other.amount))
+            return false;
+        if (country == null) {
+            if (other.country != null)
+                return false;
+        } else if (!country.equals(other.country))
+            return false;
+        if (createdBy == null) {
+            if (other.createdBy != null)
+                return false;
+        } else if (!createdBy.equals(other.createdBy))
+            return false;
+        if (createdOn == null) {
+            if (other.createdOn != null)
+                return false;
+        } else if (!createdOn.equals(other.createdOn))
+            return false;
+        if (description == null) {
+            if (other.description != null)
+                return false;
+        } else if (!description.equals(other.description))
+            return false;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (institute == null) {
+            if (other.institute != null)
+                return false;
+        } else if (!institute.equals(other.institute))
+            return false;
+        if (level == null) {
+            if (other.level != null)
+                return false;
+        } else if (!level.equals(other.level))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (student == null) {
+            if (other.student != null)
+                return false;
+        } else if (!student.equals(other.student))
+            return false;
+        if (updatedBy == null) {
+            if (other.updatedBy != null)
+                return false;
+        } else if (!updatedBy.equals(other.updatedBy))
+            return false;
+        if (updatedOn == null) {
+            if (other.updatedOn != null)
+                return false;
+        } else if (!updatedOn.equals(other.updatedOn))
+            return false;
+        if (website == null) {
+            if (other.website != null)
+                return false;
+        } else if (!website.equals(other.website))
+            return false;
+        return true;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return "Scholarship [id=" + id + ", country=" + country + ", institute=" + institute + ", level=" + level + ", name=" + name + ", amount=" + amount + ", description="
+                        + description + ", student=" + student + ", website=" + website + ", createdOn=" + createdOn + ", updatedOn=" + updatedOn + ", createdBy=" + createdBy
+                        + ", updatedBy=" + updatedBy + "]";
+    }
+
+    /**
+     * @return the instituteName
+     */
+    @Transient
+    public String getInstituteName() {
+        return instituteName;
+    }
+
+    /**
+     * @param instituteName the instituteName to set
+     */
+    public void setInstituteName(String instituteName) {
+        this.instituteName = instituteName;
+    }
+
+    /**
+     * @return the countryName
+     */
+    @Transient
+    public String getCountryName() {
+        return countryName;
+    }
+
+    /**
+     * @param countryName the countryName to set
+     */
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    /**
+     * @return the levelName
+     */
+    @Transient
+    public String getLevelName() {
+        return levelName;
+    }
+
+    /**
+     * @param levelName the levelName to set
+     */
+    public void setLevelName(String levelName) {
+        this.levelName = levelName;
+    }
+
+}
