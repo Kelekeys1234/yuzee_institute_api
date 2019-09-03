c062cd7ce960a4f018270315e01f598037ea4ca2
Mihir Mistry <mihir@seekadegree.com>
2019-09-03 20:20:47 +0530

    Deactivating CORS

--- src/main/java/com/seeka/app/filter/SimpleCORSFilter.java
+++ src/main/java/com/seeka/app/filter/SimpleCORSFilter.java
@@ -15,7 +15,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class SimpleCORSFilter implements Filter {
 
 	private final Logger log = LoggerFactory.getLogger(SimpleCORSFilter.class);
