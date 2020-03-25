709db3e48435db9f74268cc0395a304172d6c5b2
Shashank Sharma <shashank@seekadegree.com>
2020-03-25 18:22:27 +0530

    @Shashank made changes in API for subject

---
+++ src/main/java/com/seeka/app/dto/SubjectDto.java
@@ -0,0 +1,21 @@
+package com.seeka.app.dto;
+
+public class SubjectDto {
+
+	private String id;
+	private String subjectName;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getSubjectName() {
+		return subjectName;
+	}
+	public void setSubjectName(String subjectName) {
+		this.subjectName = subjectName;
+	}
+
+}
