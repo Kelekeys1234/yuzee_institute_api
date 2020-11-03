787f475a70416dd9f1c034c7fd6bebb1ca1ac1d0
Muhammad Awais Zahid <awais@yuzee.com>
2020-11-03 10:18:56 +0500

    pr comments

--- src/main/java/com/yuzee/app/processor/InstituteProcessor.java
+++ src/main/java/com/yuzee/app/processor/InstituteProcessor.java
@@ -319,23 +319,23 @@ public class InstituteProcessor {
 		log.debug("Inside updateInstitute() method");
 		try {
 			List<InstituteElasticSearchDTO> instituteElasticDtoList = new ArrayList<>();
-			log.info("fetching institute from DB having instituteId = " + instituteId);
+			log.info("fetching institute from DB having instituteId = ", instituteId);
 			Institute oldInstitute = dao.get(instituteId);
 			Institute newInstitute = new Institute();
 			InstituteElasticSearchDTO instituteElasticSearchDto = new InstituteElasticSearchDTO();
 			log.info("Copying bean class to DTO class");
 			BeanUtils.copyProperties(instituteRequest, newInstitute);
-			if (instituteRequest.getDomesticRanking() != null
+			if (!StringUtils.isEmpty(instituteRequest.getDomesticRanking())
 					&& !instituteRequest.getDomesticRanking().equals(oldInstitute.getDomesticRanking())) {
 				log.info("DomesticRanking is not null hence saving domesticRanking History");
 				saveDomesticRankingHistory(newInstitute, oldInstitute);
 			}
-			if (instituteRequest.getWorldRanking() != null
+			if (!StringUtils.isEmpty(instituteRequest.getWorldRanking()) 
 					&& !instituteRequest.getWorldRanking().equals(oldInstitute.getWorldRanking())) {
 				log.info("WorldRanking is not null hence saving worldRanking History");
 				saveWorldRankingHistory(newInstitute, oldInstitute);
 			}
-			log.info("Start updating institute for instituteId =" + instituteId);
+			log.info("Start updating institute for instituteId =", instituteId);
 			Institute institute = saveInstitute(instituteRequest, instituteId);
 			log.info("Copying DTO class to elasticSearch DTO");
 			BeanUtils.copyProperties(instituteRequest, instituteElasticSearchDto);
@@ -1167,7 +1167,6 @@ public class InstituteProcessor {
 						.filter(e -> e.getEntityId().equals(instituteResponseDto.getId())).findFirst();
 				instituteResponseDto.setLogoUrl(logoStorage.isPresent() ? logoStorage.get().getFileURL() : null);
 			});
-
 		}
 		return instituteRequestDtos;
 	}
