ba3d6da46e1be4cbaa5c04d511addea6e70f4b40
Asif <asif@yuzee.com>
2022-08-26 01:06:16 -0700

    Revert "revertnchanges"
    This reverts commit bbf551e85fdafc79cf05acaf2d779321fac76c8d.

--- src/main/java/com/yuzee/app/bean/CourseVaccineRequirement.java
+++ src/main/java/com/yuzee/app/bean/CourseVaccineRequirement.java
@@ -1,26 +1,11 @@
 package com.yuzee.app.bean;
 
 import java.io.Serializable;
-import java.util.Date;
 import java.util.Set;
 
-import javax.persistence.Column;
-import javax.persistence.ElementCollection;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Index;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
-import org.apache.commons.lang.StringUtils;
-import org.hibernate.annotations.Cascade;
-import org.hibernate.annotations.CascadeType;
-import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -29,16 +14,21 @@ import lombok.ToString;
 @Data
 @ToString
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-/*@Table(name = "course_vaccine_requirement", indexes = {
-		@Index(name = "IDX_COURSE_ID", columnList = "course_id", unique = false) }) */
+/*
+ * @Table(name = "course_vaccine_requirement", indexes = {
+ * 
+ * @Index(name = "IDX_COURSE_ID", columnList = "course_id", unique = false) })
+ */
 public class CourseVaccineRequirement implements Serializable {
 
 	private static final long serialVersionUID = 8492390790670110780L;
-
+	@Id
+	private String id;
 	@EqualsAndHashCode.Include
 	private String description;
-
-	
+	@DBRef
+	private Course course;
+	private String auditFields;
 	@EqualsAndHashCode.Include
 	private Set<String> vaccinationIds;
 
