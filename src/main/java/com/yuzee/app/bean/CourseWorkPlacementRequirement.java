3745c462ee927af938bc6764a673e28ecaa63b8b
Asif <asif@yuzee.com>
2022-08-26 01:10:41 -0700

    Revert "Revert "revertnchanges""
    This reverts commit 6241493eb9be7e022c7910183de2486b7fb2cbbb.

--- src/main/java/com/yuzee/app/bean/CourseWorkPlacementRequirement.java
+++ src/main/java/com/yuzee/app/bean/CourseWorkPlacementRequirement.java
@@ -3,10 +3,6 @@ package com.yuzee.app.bean;
 import java.io.Serializable;
 import java.util.List;
 
-import javax.persistence.Id;
-
-import org.springframework.data.mongodb.core.mapping.DBRef;
-
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -14,16 +10,12 @@ import lombok.ToString;
 @Data
 @ToString
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-/*
- * @Table(name = "course_work_placement_requirement", indexes = {
- * 
- * @Index(name = "IDX_COURSE_ID", columnList = "course_id", unique = false) })
- */
+/*@Table(name = "course_work_placement_requirement", indexes = {
+		@Index(name = "IDX_COURSE_ID", columnList = "course_id", unique = false) }) */
 public class CourseWorkPlacementRequirement implements Serializable {
 
 	private static final long serialVersionUID = 8492390790670110780L;
-	@Id
-	private String id;
+
 	@EqualsAndHashCode.Include
 	private String description;
 
@@ -33,8 +25,5 @@ public class CourseWorkPlacementRequirement implements Serializable {
 
 	@EqualsAndHashCode.Include
 	private List<String> fields;
-	private String auditFields;
-	@DBRef
-	private Course course;
 
 }
