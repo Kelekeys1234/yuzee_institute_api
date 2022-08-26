6241493eb9be7e022c7910183de2486b7fb2cbbb
Asif <asif@yuzee.com>
2022-08-26 01:06:16 -0700

    Revert "revertnchanges"
    This reverts commit bbf551e85fdafc79cf05acaf2d779321fac76c8d.

--- src/main/java/com/yuzee/app/bean/CourseSemester.java
+++ src/main/java/com/yuzee/app/bean/CourseSemester.java
@@ -2,26 +2,11 @@ package com.yuzee.app.bean;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Index;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.persistence.UniqueConstraint;
 
-import org.apache.commons.lang.StringUtils;
-import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -30,13 +15,18 @@ import lombok.ToString;
 @Data
 @ToString
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-/*@Table(name = "course_semester", uniqueConstraints = @UniqueConstraint(columnNames = { "type", "name",
-		"course_id" }, name = "UK_SEMESTER_TYPE_NA_CI"), indexes = {
-				@Index(name = "IDX_COURSE", columnList = "course_id", unique = false) }) */
+/*
+ * @Table(name = "course_semester", uniqueConstraints
+ * = @UniqueConstraint(columnNames = { "type", "name", "course_id" }, name =
+ * "UK_SEMESTER_TYPE_NA_CI"), indexes = {
+ * 
+ * @Index(name = "IDX_COURSE", columnList = "course_id", unique = false) })
+ */
 public class CourseSemester implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
+	@Id
+	private String id;
 	@EqualsAndHashCode.Include
 	private String type;
 
@@ -46,6 +36,12 @@ public class CourseSemester implements Serializable {
 	@EqualsAndHashCode.Include
 	private String description;
 
+	private String auditFields;
+	@DBRef
+	private Course course;
+
+	private String createdBy;
+
 	private List<SemesterSubject> subjects = new ArrayList<>();
-	
+
 }
