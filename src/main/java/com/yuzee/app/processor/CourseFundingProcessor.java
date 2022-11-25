3d72b108b46c969720e3b7dab7d132f6c30a254c
kelekeys1234 <fkelenna@gmail.com>
2022-11-25 00:59:51 -0800

    build error

--- src/main/java/com/yuzee/app/processor/CourseFundingProcessor.java
+++ src/main/java/com/yuzee/app/processor/CourseFundingProcessor.java
@@ -91,7 +91,7 @@ public class CourseFundingProcessor {
 		if (!ObjectUtils.isEmpty(course)) {
 		    log.info("inserting courseFunding inside fundingId");
 			List<String>fundingId=course.getCourseFundings();
-		    fundingId.addAll(courseFundingDto.stream().map(e->e.getFundingNameId().toString()).toList());
+		    fundingId.addAll(courseFundingDto.stream().map(e->e.getFundingNameId()).toList());
 		
 			List<Course> coursesToBeSavedOrUpdated = new ArrayList<>();
 			coursesToBeSavedOrUpdated.add(course);
